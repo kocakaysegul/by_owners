1da52f1dd7f963fc73174458000b2eb000784b68
Ayşegül Koçak <aysegulkock@gmail.com>
2018-09-23 20:18:01 +0300

    Initial Commit

---
+++ byOwner-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  byOwner-app
+//
+//  Created by Ayşegül Koçak on 23.09.2018.
+//  Copyright © 2018 Ayşegül Koçak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
